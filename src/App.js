8981366b2a43805c15b2a45f79999b9f1a45da91
Kareem <kareem@gmail.com>
2022-06-23 13:50:27 +0300

    Second-Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import RightSide from './Containers/RightSide';
 import MiddleSide from './Containers/MiddleSide';
 import Nav from './Containers/Nav';
 import classes from './main.module.css';
-import RefEx from './Containers/RefEx';
+// import RefEx from './Containers/RefEx';
 
 // Assume i know post-id then i should pass it as props for components
 // Assume i don't have loading for each comp
